87ad2c61d99753aba262f407fddcb80b8f210443
ASHOK MAHARJAN <ashok997@gmail.com>
2019-09-06 17:56:13 +0000

    ran the migration to create tables

--- db/migrate/20190906013114_create_products_table.rb
+++ db/migrate/20190906013114_create_products_table.rb
@@ -1,9 +1,13 @@
 class CreateProductsTable < ActiveRecord::Migration
+  
   def change
     create_table :products do |t|
       t.string :title
       t.integer :price
+      t.boolean :is_listed?
       t.string :link  
       t.integer :user_id
+    end
   end
+  
 end
