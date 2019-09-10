46a449a68b7c4f9e30be4668572ccdea7ffbc0d5
ASHOK MAHARJAN <ashok997@gmail.com>
2019-09-10 22:26:43 +0000

    added signup form and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,21 @@ class UsersController < ApplicationController
   end
   
   get '/signup' do
-    
+    erb :signup
+  end
+  
+  post '/users' do
+    if params[:name] !="" && params[:email] !="" && params[:password] != ""
+      @user = User.create(params)
+      redirect "/users/#{@user.id}"
+    else
+      
+    end
   end
   
   get '/users/:id' do
-    "show route"
+    @user = User.find_by(id: params[:id])
+    erb :'/users/show'
   end
   
 end
