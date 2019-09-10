bcc3671d868c21763211e61ee3ed55e844afd9d1
ASHOK MAHARJAN <ashok997@gmail.com>
2019-09-10 03:11:23 +0000

    filled out get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,21 @@ class UsersController < ApplicationController
   end
   
   post '/login' do 
-  
+    @user = User.find_by(email: params[:email])
+    
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "users/#{@user.id}"
+    else
+      
+    end
   end
   
   get '/signup' do
+    
+  end
+  
+  get '/users/:id' do
     
   end
   
